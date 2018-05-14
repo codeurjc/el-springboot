1796b4f519f6ed322b899a2cb8252e3a5b20e61b
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-05-14 15:52:37 +0200

    Copy logstash-input-beats and transform to spring boot

---
+++ src/test/java/io/elastest/beats/ProtocolTest.java
@@ -0,0 +1,17 @@
+package io.elastest.beats;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by ph on 2016-06-01.
+ */
+public class ProtocolTest {
+    @Test
+    public void isVersion2Test() {
+        assertTrue(Protocol.isVersion2((byte) '2'));
+        assertFalse(Protocol.isVersion2((byte) '1'));
+    }
+}
---
+++ src/test/java/io/elastest/elspringboot/ElSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package io.elastest.elspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
